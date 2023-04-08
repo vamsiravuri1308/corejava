95bfffdc2f8f7c696453b5186fb6bf806bea64d5
19WH1A1261 <vamsikrishravuri@gmail.com>
2023-04-07 21:03:07 -0500

    Decimal and Hexa decimal values

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x = 10;
+        int y = 0x10;
+        char ch = 'a';
+        System.out.println(x+".."+y);
+    }
+}
