1227838adaa721e9772a747c90100db2252a2a9c
19WH1A1261 <vamsikrishravuri@gmail.com>
2023-04-08 14:24:42 -0500

    declaring two di,esional array

---
+++ src/arrays/twodimensionalarray.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class twodimensionalarray {
+    public static void main(String[] args){
+        int[][] x = {{10,20},{10,20,30}};
+        System.out.println(x[0][1]);
+    }
+}
