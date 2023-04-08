11a4f6ca5c89286944809629680dd2472600f1cb
19WH1A1261 <vamsikrishravuri@gmail.com>
2023-04-08 14:00:49 -0500

    test

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,6 +1,6 @@
 public class Arrays {
     public static void main(String[] args) {
-        int[] x = {1, 2, 3};
-        System.out.println(x[2]);
+        int[] x = { 1, 2, 3 };
+        System.out.println(x[0]);
     }
 }
