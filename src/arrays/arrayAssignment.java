c742e0fc3faa790e0cdd90956e478a9a58dc6293
19WH1A1261 <vamsikrishravuri@gmail.com>
2023-04-09 19:29:04 -0500

    Array assigning

--- src/arrays/arrayAssignment.java
+++ src/arrays/arrayAssignment.java
@@ -3,7 +3,7 @@ package arrays;
 public class arrayAssignment {
     public static void main(String[] args){
         int[] x = new int[]{10,20};
-        int[] y= new int[]{30,40,50,60};
+        int[] y= new int[]{30,40,50,600};
         x = y;
         for (int i=0; i<x.length; i++) {
             System.out.println(x[i]);
