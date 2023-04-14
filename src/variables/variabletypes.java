f860b65c64d5c3d59191b73f3a5cad6403d96fb8
19WH1A1261 <vamsikrishravuri@gmail.com>
2023-04-13 23:22:34 -0500

    adding static and instant var types

---
+++ src/variables/variabletypes.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class variabletypes {
+    int x = 10;
+    static int y = 20;
+
+    public static void main(String[] args){
+        variabletypes t1 = new variabletypes();
+        System.out.println(t1.x);
+        System.out. println(variabletypes.y);
+    }
+}
